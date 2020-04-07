b61959cf62911443a72f7d12e2c579ad91119b42
coskun cakir <csknckr@gmail.com>
2020-04-07 21:56:21 +0100

    react native versiyon downgrade to 0.61.5 from 0.62.0 and home search screen animation implemented

--- src/components/search.js
+++ src/components/search.js
@@ -12,21 +12,8 @@ function SearchBox({ onChangeFocus }) {
   const [isFocus, setFocus] = React.useState(false)
 
   React.useEffect(() => {
-    Keyboard.addListener('keyboardDidShow', keyboardDidShow)
-    Keyboard.addListener('keyboardDidHide', keyboardDidHide)
-    return function () {
-      Keyboard.removeListener('keyboardDidShow', keyboardDidShow)
-      Keyboard.removeListener('keyboardDidHide', keyboardDidHide)
-    }
-  })
-
-  const keyboardDidShow = () => {
-    onChangeFocus(true)
-  }
-
-  const keyboardDidHide = () => {
-    onChangeFocus(false)
-  }
+    onChangeFocus(isFocus)
+  }, [isFocus])
 
   const onCancel = () => {
     setFocus(false)
--- src/views/search.js
+++ src/views/search.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Text, ImageBackground, StatusBar } from 'react-native'
+import { Text, ImageBackground, StatusBar, Animated } from 'react-native'
 import { Logo } from '../components/icons'
 import Search from '../components/search'
 import Box from '../components/box'
@@ -8,40 +8,73 @@ import SafeAreaView from 'react-native-safe-area-view'
 import { useFocusEffect } from '@react-navigation/native'
 
 function SearchView() {
+  const [heroHeight] = React.useState(new Animated.Value(285))
   const [isSearchFocus, setSearchFocus] = React.useState(false)
+
+  React.useEffect(() => {
+    if (isSearchFocus) {
+      Animated.timing(heroHeight, {
+        toValue: 84,
+        duration: 230
+      }).start()
+    } else {
+      Animated.timing(heroHeight, {
+        toValue: 285,
+        duration: 230
+      }).start()
+    }
+  }, [heroHeight, isSearchFocus])
 
   useFocusEffect(
     React.useCallback(() => {
-      StatusBar.setBarStyle('light-content')
-    }, [])
+      StatusBar.setBarStyle(isSearchFocus ? 'dark-content' : 'light-content')
+    }, [isSearchFocus])
   )
 
   return (
-    <Box as={SafeAreaView} bg="red" flex={1}>
+    <Box as={SafeAreaView} bg={isSearchFocus ? 'softRed' : 'red'} flex={1}>
       {/** Header */}
-      <Box position="relative" zIndex={1} height={isSearchFocus ? 0 : 285}>
+      <Box
+        as={Animated.View}
+        position="relative"
+        zIndex={1}
+        height={heroHeight}
+      >
+        {!isSearchFocus && (
+          <Box
+            as={ImageBackground}
+            source={bg}
+            style={{ width: '100%', height: '100%' }}
+          >
+            {/** Logo */}
+            <Box flex={1} alignItems="center" justifyContent="center">
+              <Logo color="white" />
+            </Box>
+          </Box>
+        )}
+        {/** Search Box */}
         <Box
-          as={ImageBackground}
-          source={bg}
-          style={{ width: '100%', height: '100%' }}
+          position="absolute"
+          left={0}
+          bottom={isSearchFocus ? 0 : -42}
+          p={16}
+          width="100%"
         >
-          {/** Logo */}
-          <Box flex={1} alignItems="center" justifyContent="center">
-            <Logo color="white" />
-          </Box>
-
-          {/** Search Box */}
-          <Box p={16} width="100%" mb={-42}>
-            <Search onChangeFocus={(status) => setSearchFocus(status)} />
-          </Box>
+          <Search onChangeFocus={(status) => setSearchFocus(status)} />
         </Box>
       </Box>
 
       {/** Content */}
       <Box flex={1} bg="white" pt={26}>
-        <Box p={30} flex={1}>
-          <Text>Hello</Text>
-        </Box>
+        {isSearchFocus ? (
+          <Box p={30} flex={1}>
+            <Text>History</Text>
+          </Box>
+        ) : (
+          <Box p={30} flex={1}>
+            <Text>Hello</Text>
+          </Box>
+        )}
       </Box>
     </Box>
   )
